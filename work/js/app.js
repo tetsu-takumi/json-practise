3288f910a0637628b5fb9aa40c4e8012fbd20e5e
tetsu-takumi <detsu.0113.soccer@icloud.com>
2025-07-24 23:32:32 +0900

    表作成

--- work/js/app.js
+++ work/js/app.js
@@ -1,11 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     fetch('data/product.json')
     .then(response => response.json())
     .then(data => {
         data.forEach(product => {
-            const row = document.createElement('tr');
-            row.innerHTML = `<td>${product.id}</td><td>${product.name}</td><td>${product.price}</td><td>${product.description}</td>`;
-            document.getElementById('productTable').appendChild(row);
+            const tr = Object.assign(document.createElement('tr'), {
+                innerHTML: `
+                <td>${product.id}</td>
+                <td>${product.name}</td>
+                <td>${product.price}</td>
+                <td>${product.description}</td>`
+            });
+            document.getElementById('productTable').appendChild(tr);
         });
     })
     .catch(error => console.error('エラー:', error));
